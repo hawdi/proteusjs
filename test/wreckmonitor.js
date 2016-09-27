19f0ff22297b60de97829f85be59b46341d644a4
Jai Kishan <kishan.jai1991@gmail.com>
2016-09-27 23:19:34 +0530

    add test for wreck request error.

--- test/wreckmonitor.js
+++ test/wreckmonitor.js
@@ -35,6 +35,7 @@ const plugin = {
 const wreckReq = require('./fixtures/wreckrequest');
 const wreckRes = require('./fixtures/wreckresponse');
 const wreckUri = require('./fixtures/wreckuri');
+const wreckFixtures = require('./fixtures/wreck');
 
 describe('Monitor :: Wreck', () => {
 
@@ -69,6 +70,19 @@ describe('Monitor :: Wreck', () => {
     });
 
     wreck.emit('response', null, wreckReq, wreckRes, null, wreckUri);
+  });
+
+  it('wreck request error', {plan: 4}, (done) => {
+    plugin.options.reporters.logit.once('logit', function(result){
+
+      expect(result.object).equal('wreck');
+      expect(result.event).equal('response');
+      expect(result.statusCode).equal(wreckFixtures.error.output.statusCode);
+      expect(result.statusMessage).to.be.equal(wreckFixtures.error.output.payload.message);
+      done();
+    });
+
+    wreck.emit('response', wreckFixtures.error, wreckReq, null, null, wreckUri);
   });
 
   it('disable request and response handler', (done) => {
