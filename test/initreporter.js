19e55216f3149764b291dc14acb1f87aec7860f5
Jai Kishan <kishan.jai1991@gmail.com>
2016-09-29 01:02:45 +0530

    more test for initial reporter.

--- test/initreporter.js
+++ test/initreporter.js
@@ -24,7 +24,7 @@ const plugin = {
 
 describe('Reporter :: Console', () => {
 
-  it('pass initialized console reporter', (done) => {
+  it('pass uninitialized console reporter', {plan: 2}, (done) => {
 
     function ProteusjsStream() {
 
@@ -39,11 +39,43 @@ describe('Reporter :: Console', () => {
     Util.inherits(ProteusjsStream, Stream.Transform);
 
     ProteusjsStream.prototype._transform = function (data, encoding, next) {
+
+      expect(data).to.be.an.object();
       expect(data.test).equal('yes');
       next(done());
     };
 
     plugin.options.reporters.console = ProteusjsStream;
+    Reporter.init(plugin.options);
+    plugin.options.reporters.logit.emit('logit', { test : 'yes' });
+
+  });
+
+  it('pass initialized console reporter', {plan: 2}, (done) => {
+
+    function ProteusjsStream() {
+
+      if (!(this instanceof ProteusjsStream)) {
+        return new ProteusjsStream({ objectMode: true });
+      }
+
+      // init Transform
+      Stream.Transform.call(this, { objectMode: true });
+    };
+
+    Util.inherits(ProteusjsStream, Stream.Transform);
+
+    ProteusjsStream.prototype._transform = function (data, encoding, next) {
+
+      expect(data).to.be.an.object();
+      expect(data.test).equal('yes');
+      next(done());
+    };
+
+    const proteusjsStream = new ProteusjsStream();
+
+    plugin.options.reporters.console = proteusjsStream;
+    plugin.options.reporters.logit = null;
     Reporter.init(plugin.options);
     plugin.options.reporters.logit.emit('logit', { test : 'yes' });
 
